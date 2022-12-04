3fc652f1a87ecd0d794a4aff7d6247f88627bb59
Jake Correnti <jcorrenti13@outlook.com>
2022-12-03 21:18:30 -0500

    day 3

---
+++ 2022/day3/go.mod
@@ -0,0 +1,3 @@
+module aoc/2022/day3
+
+go 1.17
---
+++ 2022/day3/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	bytes, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	split := strings.Split(string(bytes), "\n")
+
+	shared := []rune{}
+	// Part 1
+
+	//for _, s := range split {
+	//if len(s) == 0 {
+	//break
+	//}
+	//s1 := s[:len(s)/2]
+	//s2 := s[len(s)/2:]
+	//for _, c := range s1 {
+	//if strings.Contains(s2, string(c)) {
+	//shared = append(shared, c)
+	//break
+	//}
+	//}
+	//}
+
+	//total := 0
+	//for _, s := range shared {
+	//if unicode.IsLower(s) {
+	//total += int(s-'a') + 1
+	//} else {
+	//total += int(s-'A') + 27
+	//}
+	//}
+	//fmt.Println(total)
+
+	for i := 0; i < len(split)-3; i += 3 {
+		s1 := split[i]
+		s2 := split[i+1]
+		s3 := split[i+2]
+		for _, c := range s1 {
+			if strings.Contains(s2, string(c)) && strings.Contains(s3, string(c)) {
+				shared = append(shared, c)
+				break
+			}
+		}
+	}
+
+	total := 0
+	for _, s := range shared {
+		if unicode.IsLower(s) {
+			total += int(s-'a') + 1
+		} else {
+			total += int(s-'A') + 27
+		}
+	}
+	fmt.Println(total)
+}
